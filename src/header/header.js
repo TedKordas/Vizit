05c7aa443843675aa61ef2b8c83e2d2e42605289
Ted Kordas <tedleekordas@gmail.com>
2019-05-29 23:30:50 -0600

    This is before my flight

---
+++ src/header/header.js
@@ -0,0 +1,14 @@
+import react, { Component } from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+import Vizit from '../pics/Vizit.jpeg';
+
+class header extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
